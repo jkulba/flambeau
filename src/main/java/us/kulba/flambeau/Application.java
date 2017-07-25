a9f133efe5045f29754243e9c3511e44b66abffb
James Kulba <James.Kulba@thermofisher.com>
2017-07-24 20:17:19 -0700

    rename main driver

--- src/main/java/us/kulba/flambeau/Application.java
+++ src/main/java/us/kulba/flambeau/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FlambeauApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FlambeauApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
