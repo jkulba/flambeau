cb0dbeb48b1e72d6349ea15af18ddfcf240b75e0
James Kulba <James.Kulba@thermofisher.com>
2017-07-24 21:19:03 -0700

    update to run

--- src/main/java/us/kulba/flambeau/Application.java
+++ src/main/java/us/kulba/flambeau/Application.java
@@ -1,12 +1,25 @@
 package us.kulba.flambeau;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+	final Logger logger = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+        logger.info("Hello Joe!");
 	}
 }
