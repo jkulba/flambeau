5a365675fddedd859ee90a23203ac49b89d66e3f
James Kulba <James.Kulba@thermofisher.com>
2017-07-24 19:59:36 -0700

    add new project files

---
+++ src/test/java/us/kulba/flambeau/FlambeauApplicationTests.java
@@ -0,0 +1,16 @@
+package us.kulba.flambeau;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlambeauApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
